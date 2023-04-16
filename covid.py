32fe158299ddda1797207765382a62f36d8d5865
teeruchtheprungsirikul <teeruch_th@hotmail.com>
2023-04-16 05:26:53 -0700

    sleep

--- covid.py
+++ covid.py
@@ -1,12 +1,12 @@
 import requests
 import json
 
-proxies = {
-        "http" : 'covid19.ddc.moph.go.th:443',
-        "https" : 'covid19.ddc.moph.go.th:443'
-}
+# proxies = {
+#         "http" : 'covid19.ddc.moph.go.th:443',
+#         "https" : 'covid19.ddc.moph.go.th:443'
+# }
 # Create a variable to store covid-19 data(Retrieve from API)
-url = requests.get('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all', proxies=proxies)
+url = requests.get('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all')
 #print(url) <Response [200]>
 print(url.content.decode("utf-8"))
 
